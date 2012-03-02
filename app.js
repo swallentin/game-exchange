169c88f030ff88bbd52c6aaaf66117eacb187120
Stephan Wallentin <stephan.wallentin@ef.com>
2012-03-03 02:33:06 +0800

    Added missing variables. Added support for configuration. Partly used by everyauth. Now using everyauth and its facebook module for login. Added currently broken for CRUD GameProdiver (memory based, mongodb through mongoose is the plan).
    Next iteration will be working on presenting from read from memory into the screen.
    
    Notes for this check-in,
    Used video tutorials for writing database stuff. Partly mongoose but also a text based tutorial where we use a provider approach instead. Perfect approach for capturing quick development and allow for change betweeen different providers such as memory or any other data storage available.

--- app.js
+++ app.js
@@ -1,16 +1,16 @@
-
-/**
- * Module dependencies.
- */
-
 var express = require('express')
   , routes = require('./routes'),
-  everyauth = require('everyauth');
+  everyauth = require('everyauth'),
+  conf = require("./conf"),
+  gameProvider = require('./lib/gameprovider-memory');
+
+everyauth.debug = true;
 
 var app = module.exports = express.createServer();
 
 var usersById = {};
 var nextUserId = 0;
+var usersByFbId = {};
 
 function addUser (source, sourceUser) {
   var user;
@@ -42,18 +42,85 @@ everyauth
     .redirectPath('/');
 
 
+    everyauth
+      .password
+        .loginWith('email')
+        .getLoginPath('/login')
+        .postLoginPath('/login')
+        .loginView('login.jade')
+    //    .loginLocals({
+    //      title: 'Login'
+    //    })
+    //    .loginLocals(function (req, res) {
+    //      return {
+    //        title: 'Login'
+    //      }
+    //    })
+        .loginLocals( function (req, res, done) {
+          setTimeout( function () {
+            done(null, {
+              title: 'Async login'
+            });
+          }, 200);
+        })
+        .authenticate( function (login, password) {
+          var errors = [];
+          if (!login) errors.push('Missing login');
+          if (!password) errors.push('Missing password');
+          if (errors.length) return errors;
+          var user = usersByLogin[login];
+          if (!user) return ['Login failed'];
+          if (user.password !== password) return ['Login failed'];
+          return user;
+        })
+
+        .getRegisterPath('/register')
+        .postRegisterPath('/register')
+        .registerView('register.jade')
+    //    .registerLocals({
+    //      title: 'Register'
+    //    })
+    //    .registerLocals(function (req, res) {
+    //      return {
+    //        title: 'Sync Register'
+    //      }
+    //    })
+        .registerLocals( function (req, res, done) {
+          setTimeout( function () {
+            done(null, {
+              title: 'Async Register'
+            });
+          }, 200);
+        })
+        .validateRegistration( function (newUserAttrs, errors) {
+          var login = newUserAttrs.login;
+          if (usersByLogin[login]) errors.push('Login already taken');
+          return errors;
+        })
+        .registerUser( function (newUserAttrs) {
+          var login = newUserAttrs[this.loginKey()];
+          return usersByLogin[login] = addUser(newUserAttrs);
+        })
+
+        .loginSuccessRedirect('/')
+        .registerSuccessRedirect('/');
 
 // Configuration
 
 app.configure(function(){
+
   app.set('views', __dirname + '/views');
   app.set('view engine', 'jade');
+  app.set('view options', { layout: false });
   app.use(express.bodyParser());
   app.use(express.methodOverride());
   app.use(require('stylus').middleware({ src: __dirname + '/public' }));
+  // app.use(express.favicon());
+  app.use(express.static(__dirname + '/public'));
+  app.use(express.cookieParser());
+  app.use(express.session({ secret: 'lolcatz'}));
   app.use(everyauth.middleware());
   app.use(app.router);
-  app.use(express.static(__dirname + '/public'));
 });
 
 app.configure('development', function(){
@@ -68,7 +135,19 @@ everyauth.helpExpress(app);
 
 // Routes
 
+app.get('/game-test', function(req, res){
+  res.send('test');
+  
+  // gameProvider.findAll(function(error, docs){
+  //   res.send(docs);
+  // });
+});
+
 app.get('/', routes.index);
+
+// app.get('/', function (req, res) {
+//   res.render('home');
+// });
 
 var port = process.env.PORT || 3000;
 app.listen(port);
--- routes/index.js
+++ routes/index.js
@@ -6,3 +6,7 @@
 exports.index = function(req, res){
   res.render('index', { title: 'Welcome to Foundation using Express with Jade and Stylus.' })
 };
+
+// routes to add
+// add/edit game
+// remove game
